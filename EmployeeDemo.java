443326043a44bf621023a6a073f8f4b6c6cbba55
Ashwinipb <ashwinybhat@gmail.com>
2016-09-26 16:41:41 +0530

    Add files via upload

---
+++ EmployeeDemo.java
@@ -0,0 +1,12 @@
+package in.ac.kletech.Employee;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee ash=new Employee(1111,"Ashwini",10000,5000);
+		ash.calNetSalary();
+		// TODO Auto-generated method stub
+
+	}
+
+}
